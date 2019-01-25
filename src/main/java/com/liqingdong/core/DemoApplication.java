aafab89585f704baad7c2e7f354ece3de7d4e634
liqingdong <229436569@qq.com>
2019-01-25 18:03:18 +0800

    添加DB,Redis,Logback配置

--- src/main/java/com/liqingdong/core/DemoApplication.java
+++ src/main/java/com/liqingdong/core/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.liqingdong.demo;
+package com.liqingdong.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
